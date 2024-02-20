c7492df35534a40911ea3082738b7260d75aa1a3
anhtrung <anhtrungnguyen789@gmail.com>
2024-02-20 21:38:31 +0700

    bai 1 -> bai 3

--- BTL/obj/Debug/net8.0/BTL.AssemblyInfo.cs
+++ BTL/obj/Debug/net8.0/BTL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b07b1f69e184491b19a89c4295591bbb6f0804d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
