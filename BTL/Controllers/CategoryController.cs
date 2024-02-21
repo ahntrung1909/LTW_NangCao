31515ee4c75be88209a88924e29524edc42f191d
Nguyễn Duy Hân <85959139+hannguyen30@users.noreply.github.com>
2024-02-21 14:01:41 +0700

    dbcontext

--- BTL/Controllers/CategoryController.cs
+++ BTL/Controllers/CategoryController.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using BTL.Models;
+using BTL.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BTL.Controllers
 {
     public class CategoryController : Controller
     {
+        private DataContext _entityContext { get; }
         public IActionResult Index()
         {
             return View();
         }
+
     }
 }
--- BTL/Models/CategoryModel.cs
+++ BTL/Models/CategoryModel.cs
@@ -1,7 +1,17 @@
-﻿namespace BTL.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BTL.Models
 {
     public class CategoryModel
     {
+        [Key]
         public int Id { get; set; }
+        [Required,MinLength(4,ErrorMessage ="Yêu cầu nhập tên danh mục")]
+        public string Name { get; set; }
+        [Required, MinLength(4, ErrorMessage = "Yêu cầu nhập mô tả danh mục")]
+        public string Description { get; set; }
+        public string Slug { get; set; }
+        public int Status { get; set; }
+
     }
 }
