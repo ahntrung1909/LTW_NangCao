19e1ebf64bb233f76a967ba1fba42fffe6bab2b3
anhtrung <anhtrungnguyen789@gmail.com>
2024-02-21 09:10:54 +0700

    fix-conflix-main

--- BTL/obj/Debug/net8.0/BTL.AssemblyInfo.cs
+++ BTL/obj/Debug/net8.0/BTL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaa55460f6b4c0fcfa49abfe0290f1ae5f23e475")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
