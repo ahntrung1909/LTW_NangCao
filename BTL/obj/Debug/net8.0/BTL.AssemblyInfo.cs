5ef26cdde069a9d39d41f71b1eb62165add5cee6
tiendang09012002 <tiendang09012002@gmail.com>
2024-03-25 23:56:00 +0700

    admin

--- BTL/obj/Debug/net8.0/BTL.AssemblyInfo.cs
+++ BTL/obj/Debug/net8.0/BTL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+948bda23881aa592d1bc803ecd63efe8db3031bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
